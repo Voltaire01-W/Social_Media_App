cbd609a74378c0a1ea5e20c0611db3363bd75615
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-12 16:51:12 -0500

    Added Validation and Login routes

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    post: [
+        {
+            userHandle: 'user',
+            body: 'this is the post body',
+            createdAt: "2021-05-12T14:28:37.234Z",
+            likeCount: 5,
+            commentCount: 2,
+        }
+    ]
+}
