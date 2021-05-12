ce4e0191a552ef057d63e207ba17c53ecbda8e87
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-12 09:34:30 -0500

    Added ExpressJS and formatting response.

--- functions/index.js
+++ functions/index.js
@@ -3,36 +3,34 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const express = require("express");
+const app = express();
 
-exports.getPosts = functions.https.onRequest((req, res) => {
+app.get("/posts", (req, res) => {
     admin.firestore()
-        .collection("posts")
-        .get()
-        .then(data => {
-            let posts = [];
-            data.forEach(doc => {
-                posts.push(doc.data());
+    .collection("posts")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+        let posts = [];
+        data.forEach(doc => {
+            posts.push({
+                postId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
             });
-            return res.json(posts);
-        })
-        .catch(err => console.error(err));
-});
+        });
+        return res.json(posts);
+    })
+    .catch(err => console.error(err));
+})
 
-exports.createPost = functions.https.onRequest((req, res) => {
-        if(req.method !== 'POST') {
-            return res.status(400).json({ error: 'Method not allowed'});
-        };
+app.post("/post", (req, res) => {
     const newPost = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
@@ -46,3 +44,5 @@ exports.createPost = functions.https.onRequest((req, res) => {
             console.error(err);
         })
 });
+
+exports.api = functions.https.onRequest(app);
