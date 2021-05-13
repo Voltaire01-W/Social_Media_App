50f1482a76641f9404761f8594a82d5ae14c5939
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-12 19:39:06 -0500

    Refactoring code and reorganizing.

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAse9xg-v9oA894PGKrRhJUFpFH1ezFqU4",
+    authDomain: "socialape-14249.firebaseapp.com",
+    projectId: "socialape-14249",
+    storageBucket: "socialape-14249.appspot.com",
+    messagingSenderId: "496183491720",
+    appId: "1:496183491720:web:2ef2bbc74b9f1ecf33733d",
+    measurementId: "G-5KRD6YJEQ7"
+  };
